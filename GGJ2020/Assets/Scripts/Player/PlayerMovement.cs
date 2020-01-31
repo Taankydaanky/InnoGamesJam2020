2d1467381e2bee875cb84d57dc24b8bc2f01d501
CarinaKr <carina.kraff@t-online.de>
2020-02-01 00:13:41 +0100

    player movment (no head)

--- GGJ2020/Assets/Scripts/Player/PlayerMovement.cs
+++ GGJ2020/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,16 +7,22 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float bodySpeed, headSpeed;
+    [SerializeField] private float maxHeadDistance;
 
     private Rigidbody2D body, head;
     private ControlsMaster controlsMaster;
     private Vector2 movement;
+    private float headMinY, headMaxY;
     
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
         head = transform.GetChild(0).GetComponent<Rigidbody2D>();
+        headMinY = head.transform.localPosition.y;
+        headMaxY = head.transform.localPosition.y + maxHeadDistance;
+
+        Debug.Log("min " + headMinY + " max " + headMaxY);
     }
 
     private void OnEnable()
@@ -43,11 +49,21 @@ public class PlayerMovement : MonoBehaviour
     }
 
 
-
-    // Update is called once per frame
     void FixedUpdate()
     {
-        body.velocity = new Vector2(movement.x * bodySpeed * Time.deltaTime, body.velocity.y);
-        head.velocity = new Vector2(body.velocity.x, movement.y * headSpeed * Time.deltaTime);
+        body.velocity = new Vector2(movement.x * bodySpeed * Time.fixedDeltaTime, body.velocity.y);
+
+        float headGoalY = movement.y > 0 ? headMaxY : headMinY;
+        Vector2 headGoal = head.transform.TransformPoint(new Vector2(0, headGoalY));
+
+        //head.transform.position = Vector2.MoveTowards(head.transform.position, headGoal, movement.y * headSpeed * Time.fixedDeltaTime);
+
+        Vector2 moveDir = Vector2.MoveTowards(head.transform.position, headGoal, movement.y * headSpeed * Time.fixedDeltaTime);
+
+        if (movement.y != 0)
+        {
+            Debug.Log(head.transform.position+" global "+headGoal+" moveDir "+moveDir);
+        }
     }
+
 }
