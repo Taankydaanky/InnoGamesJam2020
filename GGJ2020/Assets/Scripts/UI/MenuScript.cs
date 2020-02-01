6a8cf7e6ce0cc90e7745c2eee616bd6fa6d8bc41
ateRstones <t.schmolzi@web.de>
2020-02-01 18:34:53 +0100

    Added menu screen and game resolve script

---
+++ GGJ2020/Assets/Scripts/UI/MenuScript.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Events;
+
+public class MenuScript : MonoBehaviour
+{
+    public UnityEvent exitEvent;
+    private ControlsMaster control;
+    void Start()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        if(control == null)
+        {
+            control = new ControlsMaster();
+            control.Player.UIAccept.performed += UIAccept;
+        }
+        control.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        control.Player.Disable();
+    }
+
+    private void UIAccept(InputAction.CallbackContext obj)
+    {
+        enabled = false;
+        // Call event
+        if(exitEvent != null)
+        {
+            exitEvent.Invoke();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
