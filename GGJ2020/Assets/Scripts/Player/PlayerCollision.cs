bbd4ba68aa2d0081abdaf3beaf0a9a2157c6321e
CarinaKr <carina.kraff@t-online.de>
2020-02-01 12:52:33 +0100

    player prefab

--- GGJ2020/Assets/Scripts/Player/PlayerCollision.cs
+++ GGJ2020/Assets/Scripts/Player/PlayerCollision.cs
@@ -6,7 +6,6 @@ public class PlayerCollision : MonoBehaviour
 {
     private PlayerMovement playerMovement;
 
-    // Start is called before the first frame update
     void Start()
     {
         playerMovement = GetComponent<PlayerMovement>();
@@ -16,13 +15,11 @@ public class PlayerCollision : MonoBehaviour
     {
         if(collision.tag == "ElevatorUp" && playerMovement.isInElevatorMoveDir == 0)
         {
-            Debug.Log("trigger enter with " + collision.name + " " + collision.tag);
             playerMovement.isInElevatorMoveDir = 1;
             playerMovement.elevatorGoal = collision.GetComponent<Elevator>().elevatorGoal;
         }
         else if(collision.tag == "ElevatorDown" && playerMovement.isInElevatorMoveDir == 0)
         {
-            Debug.Log("trigger enter with " + collision.name+" "+collision.tag);
             playerMovement.isInElevatorMoveDir = -1;
             playerMovement.elevatorGoal = collision.GetComponent<Elevator>().elevatorGoal;
         }
@@ -32,7 +29,6 @@ public class PlayerCollision : MonoBehaviour
     {
         if ((collision.tag == "ElevatorUp" || collision.tag == "ElevatorDown"))
         {
-            Debug.Log("trigger exit with " + collision.name + " " + collision.tag);
             playerMovement.isInElevatorMoveDir = 0;
         }
     }
