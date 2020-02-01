949196ebaabfeca2646564eaceece506e8cf39de
ateRstones <t.schmolzi@web.de>
2020-02-01 14:23:34 +0100

    Fixed event sequence script name

--- GGJ2020/Assets/Scripts/Events/EventSequence.cs
+++ GGJ2020/Assets/Scripts/Events/EventSequence.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class ScriptedEventSequence : Activatable
+public class EventSequence : Activatable
 {
     public ActivatableEvent[] sequence;
     public UnityEvent endedEvent;
