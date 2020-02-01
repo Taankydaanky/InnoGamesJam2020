de1a56f062f97b5c6722a22097a21fce209170bd
ateRstones <t.schmolzi@web.de>
2020-02-01 02:57:42 +0100

    Started work on event system

---
+++ GGJ2020/Assets/Scripts/Events/GeneratedEvent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratedEvent : ScriptableObject
+{
+    private Event execEvent;
+    private float time;
+
+    public bool Activate(float time)
+    {
+        if(time >= this.time)
+        {
+            execEvent.Execute();
+            return true;
+        }
+        return false;
+    }
+
+    public GeneratedEvent(Event execEvent, float time)
+    {
+        this.execEvent = execEvent;
+        this.time = time;
+    }
+}
