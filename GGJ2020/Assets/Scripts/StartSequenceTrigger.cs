c74d8ec37db249b0c53613ebe520f1e2cc8d862f
CarinaKr <carina.kraff@t-online.de>
2020-02-01 21:15:18 +0100

    start sequence on trigger

---
+++ GGJ2020/Assets/Scripts/StartSequenceTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSequenceTrigger : MonoBehaviour
+{
+    [SerializeField] private bool destroyAfterUse = true;
+
+    private Activatable activatable;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            activatable.Activate();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            activatable.Deactivate();
+            if(destroyAfterUse)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
