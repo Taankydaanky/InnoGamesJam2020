4e367983c6546f8b022fa47faf7cba66b9f664b8
ateRstones <t.schmolzi@web.de>
2020-02-01 02:01:19 +0100

    Added health management scripts (untested)

---
+++ GGJ2020/Assets/Scripts/Health/RocketSectionHealth.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "RocketSectionHealth", menuName = "ScriptableObjects/RocketSectionHealth", order = 1)]
+public class RocketSectionHealth : ScriptableObject
+{
+    public Image imageToColor;
+    public int maxHealth;
+    public int currentHealth {
+        get => currentHealth;
+        set
+        {
+            if(value < 0)
+            {
+                value = 0;
+            }
+            currentHealth = value;
+            imageToColor.color = gradient.Evaluate(((float)currentHealth) / maxHealth);
+        }
+    }
+
+    Gradient gradient;
+    GradientColorKey[] colorKey;
+    GradientAlphaKey[] alphaKey;
+
+    void Start()
+    {
+        gradient = new Gradient();
+
+        colorKey = new GradientColorKey[3];
+        colorKey[0].color = Color.red;
+        colorKey[0].time = 0.0f;
+        colorKey[1].color = Color.yellow;
+        colorKey[1].time = ((float)maxHealth - 1) / maxHealth;
+        colorKey[2].color = Color.green;
+        colorKey[2].time = 1f;
+
+        alphaKey = new GradientAlphaKey[2];
+        alphaKey[0].alpha = 1.0f;
+        alphaKey[0].time = 0.0f;
+        alphaKey[1].alpha = 1.0f;
+        alphaKey[1].time = 1.0f;
+
+        gradient.SetKeys(colorKey, alphaKey);
+        currentHealth = maxHealth;
+    }
+}
